d64830157cb1934ef1b4a951c60ff6013e996acc
sethbr11 <sethabrock@gmail.com>
2024-03-05 12:04:03 -0700

    Added json file and imported from said file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,92 @@
 import React from 'react';
 import './App.css';
+import basketballData from './CollegeBasketballTeams.json';
+
+/*
+    Mission #9 Assignment
+    IS 413 – Hilton
+    With March Madness upon us, we want to create a website that lists out
+    information about all the colleges in NCAA Basketball. The attached file contains
+    a JSON file that has information for the basketball teams:
+    https://byu.box.com/s/2wqd1pl166n4i39whcu8vircol19eozx
+    Download the JSON file and paste it into your “src” folder.
+    Using your setup in Visual Studio Code, build a website in React that has at least
+    three different functions/components in addition to the App function:
+    #1: A heading section at the top introducing the user to what the site is.
+    #2: A team “card” that contains the following information about each school:
+    • School Name
+    • Mascot Name
+    • Location (City, State)
+    #3: A list of team cards that shows all the teams on the list.
+    Incorporate the components into your website to display a list of all the college
+    basketball teams. You will need to figure out how to make the connection to the
+    JSON file to pull the data rather than building the JSON in a const in the code itself
+    like we did in the videos. (#notcoveredinthevideos)
+    Submit a link to the GitHub repository containing your project via Learning
+    Suite.
+*/
+
+const marginStyle = {
+    margin: '50px',
+};
+
+interface TeamProps {
+    school: string;
+    city: string;
+    state: string;
+    name: string;
+}
+
+class TeamCard extends React.Component<TeamProps> {
+    render() {
+        const oneTeam = this.props;
+        return (
+            <div className="card">
+                <h2>{oneTeam.school}</h2>
+                <p>Mascot: {oneTeam.name}</p>
+                <p>
+                    Location: {oneTeam.city}, {oneTeam.state}
+                </p>
+            </div>
+        );
+    }
+}
+
+function TeamList() {
+    return (
+        <div>
+            {basketballData.teams.map((oneTeam) => (
+                <TeamCard {...oneTeam} />
+            ))}
+        </div>
+    );
+}
+
+function Heading() {
+    return (
+        <div style={marginStyle}>
+            <h1>March Madness</h1>
+            <p>
+                The most exciting time of the year is upon us: March Madness!
+                Every year, the best college basketball programs in the country
+                compete in a single-elimination tournament to see who will be
+                crowned the national champion. With so many teams to keep track
+                of and standings changing every week, it can be hard to keep up
+                with all the action. That's where we come in. We've created a
+                web app that allows you to view the current standings, check the
+                schedule for upcoming games, and see the results of past games.
+                Whether you're a die-hard fan or just looking to fill out your
+                bracket, we've got you covered. Let the madness begin!
+            </p>
+        </div>
+    );
+}
 
 function App() {
     return (
         <div className="App">
-            <header>
-                <p>This site is cool</p>
-            </header>
-            <body>
-                <div>
-                    <h1>My first React App</h1>
-                </div>
-            </body>
+            <Heading />
+            <TeamList />
         </div>
     );
 }
